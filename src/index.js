591803dff8fe275149520a3cc982a89da1482111
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-07-06 12:33:09 -0700

    Added image to content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello')
+import Restaurant from './restaurant.png'
+
+const content = document.querySelector("#content")
+
+//adding image to div
+const myRestaurant = new Image();
+myRestaurant.src = Restaurant;
+content.appendChild(myRestaurant);
+
